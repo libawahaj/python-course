33c69df78086b7b017a98758b9daf53d83cdf829
libawahaj <libawahaj2010@gmail.com>
2024-11-15 17:35:53 +0500

    initial commit

---
+++ project.py
@@ -0,0 +1,15 @@
+#symmetric differences
+
+#part A
+
+set1 = {'blue','green'}
+set2 = {'blue', 'yellow'}
+
+print("The symmetric difference for ", set1, " and ", set2, " is ", set1.symmetric_difference(set2))
+
+#part B
+
+set3 = {1,2,3,4,5}
+set4 = {1,5,6,7,8,9}
+
+print("The symmetric difference for ", set3, " and ", set4, " is ", set3.symmetric_difference(set4))
