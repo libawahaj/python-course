e2c0dcc60ea664c86f8020b97b271cbe423d1cad
libawahaj <libawahaj2010@gmail.com>
2024-11-29 18:00:19 +0500

    initial commit

--- lesson 27/project.py
+++ lesson 27/project.py
@@ -1,15 +1,17 @@
 class Dogs:
 
-    def __init__(self, name, personality, life_expectancy):
+    group = "Herding group dogs: " #one class variable
+
+    def __init__(self, name, personality): #two instance variables
         self.name = name
         self.personality = personality
-        self.life_expectancy = life_expectancy
 
     def details(self):
-        print(self.name," are ", self.personality, " and are expected to live for about ", self.life_expectancy)
+        print(self.name," are ", self.personality)
 
-breed1 = Dogs("Miniature poodles", "courageous, confident, smart","10-18 years")
-breed2 = Dogs("German Shepherds","active, proud, very smart", "12-14 years")
+breed1 = Dogs("Australian Cattle Dogs", "loyal, intelligent, and tenacious")
+breed2 = Dogs("German Shepherds","active, proud, and very smart")
 
+print(Dogs.group)
 breed1.details()
 breed2.details()
