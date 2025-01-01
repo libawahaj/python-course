71574fa80a53d2ea0fa4613629a6306ab7081007
libawahaj <libawahaj2010@gmail.com>
2025-01-01 18:43:47 +0500

    initial commit

---
+++ lesson 34/act 1.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400,300))
+
+done = False
+
+while not done:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+        
+        pygame.draw.rect(screen,(0,125,255), pygame.Rect(30,30, 50,50))
+
+    pygame.display.flip()
