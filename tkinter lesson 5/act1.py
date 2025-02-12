1bc90a6854b7c29560f184931103a6eeb768cf98
libawahaj <libawahaj2010@gmail.com>
2025-02-12 17:17:14 +0400

    initial commit

---
+++ tkinter lesson 5/act1.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+window = Tk()
+window.title("Main Window")
+window.geometry('500x500')
+
+def toplevel():
+    toplevel = Toplevel()
+    toplevel.title("Top Level Window")
+    toplevel.geometry("200x200")
+
+    l1 = Label(toplevel,text="This is the top level window", bg= 'grey', fg='blue')
+    l1.pack()
+
+    toplevel.mainloop()
+
+b1 = Button(window, text = "Click here to add top window", command=toplevel)
+b1.pack()
+
+window.mainloop()
